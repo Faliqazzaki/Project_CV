c70a40f3a782f0a665018462e4b88a22c3115fa9
Faliq Azzaki <faliqazzaki@gmail.com>
2026-03-17 09:02:34 +0700

    Setup Project

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Home" />
+
+            <div>
+                <h1>Home</h1>
+                <h2>Hello world</h2>
+            </div>
+        </>
+    );
+}
